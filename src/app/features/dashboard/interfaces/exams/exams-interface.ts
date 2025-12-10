e801b684c08053294af4fdea58310ca448026219
MayarYasser5590 <MayarYasser5590@gmail.com>
2025-12-10 20:51:33 +0200

    initial

---
+++ src/app/features/dashboard/interfaces/exams/exams-interface.ts
@@ -0,0 +1,19 @@
+export interface AllExamResponse {
+  message: string;
+  metadata: {
+    currentPage: number;
+    numberOfPages: number;
+    limit: number;
+  };
+  exams: Exam[];
+}
+
+export interface Exam {
+  _id: string;
+  title: string;
+  duration: number;
+  subject: string;
+  numberOfQuestions: number;
+  active: boolean;
+  createdAt: string;
+}
--- src/app/features/dashboard/pages/exams/exams.ts
+++ src/app/features/dashboard/pages/exams/exams.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { DashboardHeader } from "../../layout/dashboard-header/dashboard-header";
 import { DashboardPagesContainer } from "../../shared/dashboard-pages-container/dashboard-pages-container";
 import { Back } from "../../shared/back/back";
-import { ExamHeader } from "./components/ui/exam-header/exam-header";
+import { AllExamResponse } from '../../interfaces/exams/exams-interface';
+import { ExamsService } from '../../services/Exams/exams.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-exams',
-  imports: [DashboardPagesContainer, DashboardHeader, Back, ExamHeader],
+  imports: [DashboardPagesContainer, DashboardHeader, Back],
   templateUrl: './exams.html',
   styleUrl: './exams.scss',
 })
 export class Exams {
+  private readonly diplomaService = inject(ExamsService)
+  Exams : AllExamResponse[] = []
+  ExamsSubscribe : Subscription = new Subscription();
 
+  
 }
