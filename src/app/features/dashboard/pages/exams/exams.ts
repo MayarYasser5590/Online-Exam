725bba564e5685503e1cafbc24b9548f53714844
MayarYasser5590 <MayarYasser5590@gmail.com>
2025-12-10 20:52:56 +0200

    improvements

--- src/app/features/dashboard/pages/exams/exams.ts
+++ src/app/features/dashboard/pages/exams/exams.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { DashboardHeader } from "../../layout/dashboard-header/dashboard-header";
 import { DashboardPagesContainer } from "../../shared/dashboard-pages-container/dashboard-pages-container";
 import { Back } from "../../shared/back/back";
-import { ExamHeader } from "./components/ui/exam-header/exam-header";
 
 @Component({
   selector: 'app-exams',
-  imports: [DashboardPagesContainer, DashboardHeader, Back, ExamHeader],
+  imports: [DashboardPagesContainer, DashboardHeader, Back],
   templateUrl: './exams.html',
   styleUrl: './exams.scss',
 })
