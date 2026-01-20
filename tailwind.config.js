e0bc93c3892f36b57688f388e7c0584bbf96720c
MayarYasser5590 <MayarYasser5590@gmail.com>
2026-01-20 09:23:39 +0200

    updates4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts}', 'node_modules/flowbite/**/*.js'],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       fontFamily: {
@@ -14,5 +14,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [],
 };
